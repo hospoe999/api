2cae9ef4309c8b7644476c51c05f6dd809882551
elvisfreire <Freireelvis335@gmail.com>
2025-07-29 14:19:56 -0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS  # Corrigido aqui
 
 app = Flask(__name__)
-COURS(app)
+CORS(app)  # Corrigido aqui
 
 @app.route("/api/saludar", methods=['POST'])
 def saludar():
-  data = request.get_json()
-  nome = data.get("nome")
-  if nome == "Elvis":
-    return jsonify({"menssagem": f"ola {nome}"})
-  else:
-    return jsonify({"menssagem": "nome nao autorizado"}), 403
+    data = request.get_json()
+    nome = data.get("nome")  # Corrigido aqui (confira se a chave é "nome" mesmo)
+    if nome == "Elvis":
+        return jsonify({"mensagem": f"ola {nome}"})  # Corrigido "menssagem" para "mensagem"
+    else:
+        return jsonify({"mensagem": "nome nao autorizado"}), 403  # Corrigido "menssagem"
+
 if __name__ == '__main__':
-  app.run()
+    app.run()
